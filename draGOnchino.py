264bf47be4cdd44123f6ef46765112f03c39e942
joaco navio <enzo.navio@alumnos.frm.utn.edu.ar>
2023-04-19 12:07:03 -0300

    Codigo parte 2

--- draGOnchino.py
+++ draGOnchino.py
@@ -1,3 +1,4 @@
+import pygame,sys
 # Constantes
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -11,3 +12,16 @@ FPS = 30
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 clock = pygame.time.Clock()
+
+
+# Funciones
+def draw_board():
+    for y in range(BOARD_SIZE):
+        for x in range(BOARD_SIZE):
+            color = WHITE
+            if (x + y) % 2 == 0:
+                color = GRAY
+            pygame.draw.rect(screen, color, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+def draw_piece(x, y, color):
+    pygame.draw.circle(screen, color, (x * CELL_SIZE + CELL_SIZE // 2, y * CELL_SIZE + CELL_SIZE // 2), CELL_SIZE // 2 - 5)
