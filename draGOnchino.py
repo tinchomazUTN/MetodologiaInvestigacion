49f394f6df45f4d7c0703de84cfd66c7e4489c08
unknown <sebastian.mancilla@alumnos.frm.utn.edu.ar>
2023-04-19 12:15:48 -0300

    Codico parte 3

--- draGOnchino.py
+++ draGOnchino.py
@@ -25,3 +25,26 @@ def draw_board():
 
 def draw_piece(x, y, color):
     pygame.draw.circle(screen, color, (x * CELL_SIZE + CELL_SIZE // 2, y * CELL_SIZE + CELL_SIZE // 2), CELL_SIZE // 2 - 5)
+    def get_cell(pos):
+    x, y = pos
+    cell_x, cell_y = x // CELL_SIZE, y // CELL_SIZE
+    if cell_x >= BOARD_SIZE or cell_y >= BOARD_SIZE:
+        return None
+    return (cell_x, cell_y)
+
+# Tablero
+board = [[None for x in range(BOARD_SIZE)] for y in range(BOARD_SIZE)]
+
+# Bucle principal
+while True:
+    # Eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            cell = get_cell(pos)
+            if cell is not None:
+                x, y = cell
+                board[x][y] = BLACK
