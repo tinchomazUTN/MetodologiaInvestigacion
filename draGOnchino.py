13f8cbc363be88c6266fa04df3ac06420e8a1966
Martin Flores <juanm.flores@alumnos.frm.utn.edu.ar>
2023-05-25 03:47:37 -0300

    Pantalla completa,Aumento tamaño de tablero,Salir con el boton escape

--- draGOnchino.py
+++ draGOnchino.py
@@ -16,8 +16,8 @@ Negro = (0, 0, 0)
 Blanco = (255,255,255)
 #color del tablero
 ColorTablero = (125,125,125)
-
-
+sp= 38
+alt=703
 class nuevoSprite(pygame.sprite.Sprite):
     def __init__(self, array_indexes, location, size, color):
         super(nuevoSprite, self).__init__()
@@ -35,8 +35,8 @@ class Main:
         #inicializar la biblioteca pygame y prepararla para su uso
         pygame.init()
         #Dimensiones de la pantalla
-        anchoDePantalla = 1000
-        altoDePantalla = 700
+        anchoDePantalla = 1280
+        altoDePantalla = 720
 
         #Asignamos un objeto de Sprites
         self.sprites = pygame.sprite.Group()
@@ -49,7 +49,7 @@ class Main:
 
 
         #creación de una ventana de visualización
-        self.screen = pygame.display.set_mode((anchoDePantalla, altoDePantalla))
+        self.screen = pygame.display.set_mode((anchoDePantalla, altoDePantalla),pygame.FULLSCREEN)
         #La siguiente linea indica el turno de cada jugador
         pygame.display.set_caption('PELEA! | Comienza Dragon Negro')
         """
@@ -71,10 +71,10 @@ class Main:
         # inicializar la biblioteca pygame y prepararla para su uso
         pygame.init()
         # Dimension de la pantalla de inicio
-        screen = pygame.display.set_mode((1000, 700))
+        screen = pygame.display.set_mode((1280, 720),pygame.FULLSCREEN)
         #Imagen inicial de fondo
         background_image = pygame.image.load("lib/portada.png").convert()
-        background_image = pygame.transform.scale(background_image, (1000, 700))
+        background_image = pygame.transform.scale(background_image, (1280, 720))
 
         #Boton
         button_image = pygame.image.load("lib/boton.png").convert_alpha()
@@ -95,12 +95,15 @@ class Main:
         pygame.display.flip()
 
         #Bucle de pantalla de inicion para capturar los eventos del teclado o mouse
-        while True:
+        Ejecutando = True
+        while Ejecutando:
             for event in pygame.event.get():
                 #Si se presiona el boton de salir(X) se cierra el juego
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
+                    Ejecutando = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        Ejecutando = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     """
                     Se determina si el evento esta sobre la posicion del boton,
@@ -133,51 +136,45 @@ class Main:
                 Dibujamos las ubicaciones de los Sprites
                 """
                 self.dibujarSprites()
-
                 if event.type == MOUSEBUTTONUP:
                     #posición actual del cursor del mouse en la ventana del juego (x,y)
                     pos = pygame.mouse.get_pos()
                     #contiene los sprites del grupo self.sprites con los que el cursor del mouse ha colisionado.
                     clicked_sprites = [sprite for sprite in self.sprites if self.spriteClick(sprite.location, pos)]
-
                     #asegurarse de que se ha hecho clic en al menos un sprite
                     if clicked_sprites:
                         clicked_sprite = clicked_sprites[0]
-
                         #verificar si el sprite clikeado no está ocupado.
                         if not clicked_sprite.occupied:
                             self.turno += 1
                             #colorcirculo es negro si el numero es impar y blanco si es par
                             colorCirculo = Negro if self.turno % 2 else Blanco
-
                             #obtener las coordenadas x , y de la ubicación del sprite clikeado.
                             x, y = clicked_sprite.location
                             posicion = (x + 1, y)
-
                             #dibuja un círculo en la pantalla en la posición loc, con un radio de 10 píxeles y utilizando el colo
                             pygame.draw.circle(self.screen, colorCirculo, posicion, 10, 0)
-
                             clicked_sprite.occupied = True
                             clicked_sprite.color = colorCirculo
                     print()
-
                 elif event.type == QUIT:
                     ejecutando = False
-
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        ejecutando= False
             pygame.display.update()
-
         pygame.quit()
 
     def ubicacionSprites(self):
         #lista para las ubicaciones de los sprites
-        ubicaciones = []
+        locations = []
 
-        for y_index, y_pos in enumerate(range(10, 551, 30)):
-            for x_index, x_pos in enumerate(range(10, 551, 30)):
-                ubicaciones.append([[y_index, x_index], [y_pos, x_pos]])
+        for y_index, y_pos in enumerate(range(10, alt, sp)):
+            for x_index, x_pos in enumerate(range(10, alt, sp)):
+                locations.append([[y_index, x_index], [y_pos, x_pos]])
 
         #se guarda la lista en la variable de clase
-        self.locations = ubicaciones
+        self.locations = locations
 
     def ubicarSprites(self):
         #rastrear la fila y el índice del elemento en la matriz
@@ -203,18 +200,17 @@ class Main:
     #Metodo para Dibujar el tablero
     def dibujarTablero(self):
 
-        for y_pos in range(10, 551, 30):
-            pygame.draw.line(self.screen, Negro, (10, y_pos), (551, y_pos), width=2)
-
-        for x_pos in range(10, 551, 30):
-            pygame.draw.line(self.screen, Negro, (x_pos, 10), (x_pos, 551), width=2)
+        for y_pos in range(10, alt,sp):
+            pygame.draw.line(self.screen, Negro, (10, y_pos), (alt, y_pos), width=2)
+        for x_pos in range(10, alt, sp):
+            pygame.draw.line(self.screen, Negro, (x_pos, 10), (x_pos, alt), width=2)
 
     def dibujarSprites(self):
         for entity in self.sprites:
             if entity.occupied:
                 x, y = entity.location
                 loc = (x+1, y)
-                pygame.draw.circle(self.screen, entity.color, loc, 10, 0)
+                pygame.draw.circle(self.screen, entity.color, loc, 15, 0)
 
     def spriteClick(self, posicion_sprite, posicion_click):
         sprite_y, sprite_x = posicion_sprite
@@ -231,4 +227,3 @@ class Main:
 if __name__ == '__main__':
     app = Main()
     app.pantallaInicio()
-    
