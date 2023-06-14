e546c180874abd600fff207c09757994f7712785
Martin Flores <juanm.flores@alumnos.frm.utn.edu.ar>
2023-06-13 21:50:47 -0300

    reajustes

--- draGOnchino.py
+++ draGOnchino.py
@@ -19,8 +19,8 @@ Negro = (0, 0, 0)
 Blanco = (255,255,255)
 #color del tablero
 ColorTablero = (125,125,125)
-sp= 33
-alt=608
+sp= 32
+alt=589
 imagen1 = pygame.image.load("lib/FichaNegra.png")
 imagen2 = pygame.image.load("lib/FichaBlanca.png")
 imagen1 = pygame.transform.scale(imagen1, (40, 40))
@@ -153,7 +153,7 @@ class Main:
         pygame.init()
         #Dimensiones de la pantalla
         anchoDePantalla = 620
-        altoDePantalla = 740
+        altoDePantalla = 690
 
         # Asignamos un objeto de Sprites
         self.sprites = pygame.sprite.Group()
@@ -207,21 +207,21 @@ class Main:
         # IMAGEN QUE MUESTRA DE QUIEN ES EL TURNO
 
         turnoImagenN = pygame.image.load('lib/turnocolorN.png')
-        turnoImagenN = pygame.transform.scale(turnoImagenN, (318, 111))
+        turnoImagenN = pygame.transform.scale(turnoImagenN, (190, 90))
         turnoImagenB = pygame.image.load('lib/turnocolorB.png')
-        turnoImagenB = pygame.transform.scale(turnoImagenB, (318, 111))
-        ubicacionTurno = self.screen.get_width() - 610
+        turnoImagenB = pygame.transform.scale(turnoImagenB, (190, 90))
+        ubicacionTurno = self.screen.get_width() - 500
         #Botones para pasar menu y rendirse
         botonPasarImagen = pygame.image.load("lib/pasar.png").convert_alpha()
-        botonPasarImagen = pygame.transform.scale(botonPasarImagen, (178, 52))
+        botonPasarImagen = pygame.transform.scale(botonPasarImagen, (142, 40))
         botonPasar = botonPasarImagen.get_rect()
         botonPasar.x = self.screen.get_width() - 240
-        botonPasar.y = 620
+        botonPasar.y = 600
         botonMenuImagen = pygame.image.load("lib/menu.png").convert_alpha()
-        botonMenuImagen = pygame.transform.scale(botonMenuImagen, (178, 52))
+        botonMenuImagen = pygame.transform.scale(botonMenuImagen, (142, 40))
         botonMenu = botonMenuImagen.get_rect()
         botonMenu.x = self.screen.get_width() - 240
-        botonMenu.y = 680
+        botonMenu.y = 645
         # Creamos el fondo de la pantalla
         self.screen.blit(background_image, (0, 0))
         # Dibujamos el tablero
@@ -235,7 +235,7 @@ class Main:
 
         while ejecutando:
             clock.tick(fps)
-            self.screen.blit(turnoImagenN, (ubicacionTurno, 620))
+            self.screen.blit(turnoImagenN, (ubicacionTurno, 595))
             if self.gameover:
                 ejecutando = False
                 if self.calculateWhoWon() == 'White':
